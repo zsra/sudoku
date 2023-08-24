c26bc737a5ba9a9319abaa34d6f67f021a5ececd
richard <richard.zsupos@gmail.com>
2023-08-24 23:18:03 +0200

    Add functions to the buttons

--- main.js
+++ main.js
@@ -1,46 +1,113 @@
-var button = document.querySelector(".start-box");
+var startButton = document.querySelector(".start-box");
 var gameContainer = document.querySelector(".game-container");
 var table = document.querySelector(".sudoku-table");
 var tableBody = document.querySelector(".sudoku-table-body");
+var timer = document.querySelector(".timer");
+var newGame = document.querySelector(".new-game");
+var resetGame = document.querySelector(".reset-game");
+var radioButtons = document.querySelectorAll('input[name="difficulty"]');
+
+var hours = 0;
+var minutes = 0;
+var seconds = 0;
+
+var solution = {};
+var puzzle = generateSudoku();
 
 
-button.addEventListener('click', () => {
+startButton.addEventListener('click', () => {
   showTable();
-
-  let puzzle = generateSudoku();
-
   createCells(puzzle);
+  startTimer();
 });
 
+resetGame.addEventListener('click', () => {
+ 
+  puzzle = setPuzzleWithEmptyCells(solution);
+
+  UpdateTable();
+  resetTimer();
+});
+
+newGame.addEventListener('click', () => {
+  puzzle = generateSudoku();
+  
+  UpdateTable();
+  resetTimer();
+});
+
+function UpdateTable() {
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+
+      if (table.rows[i].cells[j].className.includes("empty")) {
+        table.rows[i].cells[j].classList.remove("empty");
+        table.rows[i].cells[j].removeChild(table.rows[i].cells[j].children[0]);
+      } else {
+        table.rows[i].cells[j].innerHTML = "";
+      }
+
+      if (puzzle[i][j] !== 0) {
+        table.rows[i].cells[j].innerHTML = puzzle[i][j];
+      } else {
+        table.rows[i].cells[j].className = "empty";
+        let input = document.createElement('input');
+        table.rows[i].cells[j].appendChild(input);
+      }
+    }
+  }
+}
 
 function showTable() {  
-  button.style.visibility = "collapse";
+  startButton.style.visibility = "collapse";
   gameContainer.style.visibility = "visible";
 }
 
 function createCells(puzzle) {
   for(let i = 0; i < 9; i++) {
-      let row = table.insertRow(i);
+      
+    let row = table.insertRow(i);
 
-      for(let j = 0; j < 9; j++) {
-          let cell = row.insertCell(j);
+    for(let j = 0; j < 9; j++) {
+      let cell = row.insertCell(j);
 
-          if(puzzle[i][j] !== 0) {
-              cell.innerHTML = puzzle[i][j];
-          } else {
-            let input = document.createElement('input');
-            cell.appendChild(input);
-          }
+      if(puzzle[i][j] !== 0) {
+        cell.innerHTML = puzzle[i][j];
+      } else {
+        cell.className = "empty";
+        let input = document.createElement('input');
+        cell.appendChild(input);
       }
+    }
   }
 }
+
 function generateSudoku() {
   let grid = Array.from({ length: 9 }, () => Array(9).fill(0));
 
   fillDiagonalSubgrids(grid);
   solveSudoku(grid);
 
-  return removeNumbers(grid, 0.5);
+  solution = grid;
+  return setPuzzleWithEmptyCells(grid);
+}
+
+function setPuzzleWithEmptyCells(grid) {
+  let difficulty = selectDifficulty();
+  let diffValue = 0.5;
+
+  if (difficulty === "easy") {
+    diffValue = 0.3;
+  }
+  else if (difficulty === "hard") {
+    diffValue = 0.7;
+  }
+
+  let copy = grid.map(function(arr) {
+    return arr.slice();
+  });
+
+  return removeNumbers(copy, diffValue);
 }
 
 function fillDiagonalSubgrids(grid) {
@@ -140,7 +207,6 @@ function removeNumbers(grid, difficulty) {
       grid[row][col] = 0;
 
       const copy = grid.map(row => [...row]);
-
       if (!solveSudoku(copy)) {
         grid[row][col] = temp;
         i--;
@@ -151,4 +217,45 @@ function removeNumbers(grid, difficulty) {
   }
 
   return grid;
+}
+
+function startTimer() {
+
+  timerInterval = setInterval(function () {
+    seconds++;
+
+    if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+    }
+
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+
+    const formattedTime = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
+    timer.textContent = formattedTime;
+  }, 1000);
+}
+
+function resetTimer() {
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+}
+
+function pad(num) {
+  return num.toString().padStart(2, '0');
+}
+
+function selectDifficulty() {
+  let value = "";
+  radioButtons.forEach(function(radioButton) {
+    if (radioButton.checked) {
+      value =  radioButton.value.toString();
+  }
+  });
+
+  return value;
 }
